c6472153d3a4f11f81ca747d2b94101a95e6d23b
Shreyas Dhoke <shreyas.d@browserstack.com  git config --global user.name Shreyas>
2025-07-25 13:04:46 +0530

    reupdated some file

--- seed.groovy
+++ seed.groovy
@@ -1,7 +1,1 @@
-def jobFolder = new File("${JENKINS_HOME}/workspace/seed-job/jenkins/jobs")
-
-jobFolder.eachFile { job ->
-    if (job.name.endsWith(".groovy")) {
-        evaluate job
-    }
-}
+jobDsl targets: 'jenkins/jobs/*.groovy'
